6b6947a37ed297e583afa8de282d41e4f6eb4a1b
Silvina Lasar <31020685+silvinalasar16@users.noreply.github.com>
2025-03-17 21:35:22 -0300

    Creacion de variables en app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+//Creación de variabes globales para la aplicación de Amigo Secreto
+let amigos = []; // se guardaran los amigos que se vayan ingresando en el sistema
+let sorteoRealizado = false; // controlar si el sorteo ya fue realizado
+
+
